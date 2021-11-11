6e4e29c40798488cf81939deb7a8f2c32fc3bf9c
Lea <62440768+Lea1216@users.noreply.github.com>
2021-11-11 13:47:18 +0200

    Create new.js

---
+++ new.js
@@ -0,0 +1,98 @@
+{
+    "name": "countries_hits",
+     "children": [
+        {
+          "name": "CN",
+          "size": 151685,
+          
+            "children": [
+              {
+                "name": "Rule1",
+                "size": 86036
+              },
+              {
+                "name": "Rule2",
+                "size": 30016
+              },
+              {
+                "name": "Rule3",
+                "size": 25680
+              },
+              {
+                "name": "Rule4",
+                "size": 4389
+              },
+              {
+                "name": "Rule5",
+                "size": 3189
+              },
+              {
+                "name": "Rule6",
+                "size": 2768
+              },
+              {
+                "name": "Rule7",
+                "size": 830
+              },
+              {
+                "name": "Rule8",
+                "size": 612
+              },
+              {
+                "name": "Rule9",
+                "size": 395
+              },
+              {
+                "name": "Rule10",
+                "size": 254
+              }
+            ]
+        },
+        {
+          "name": "uncat",
+          "size": 8075,
+            "children": [
+              {
+                "name": "Rule11",
+                "size": 4371
+              },
+              {
+                "name": "Rule12",
+                "size": 2459
+              },
+              {
+                "name": "Rule13",
+                "size": 1492
+              },
+              {
+                "name": "Rule14",
+                "size": 156
+              },
+              {
+                "name": "Rule15",
+                "size": 18
+              },
+              {
+                "name": "Rule16",
+                "size": 9
+              },
+              {
+                "name": "Rule17",
+                "size": 5
+              },
+              {
+                "name": "Rule18",
+                "size": 4
+              },
+              {
+                "name": "Rule19",
+                "size": 3
+              },
+              {
+                "name": "Rule20",
+                "size": 3
+              }
+            ]
+        }
+      ]
+  }
